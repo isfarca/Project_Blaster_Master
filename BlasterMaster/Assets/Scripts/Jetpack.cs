b6d770a8f0423f9f0c832c123fba89efa3c56bfd
Fethi Isfarca <fisfarca@googlemail.com>
2018-09-05 19:56:20 +0200

    Jetpack logic finished and grababble optimizing

--- BlasterMaster/Assets/Scripts/Jetpack.cs
+++ BlasterMaster/Assets/Scripts/Jetpack.cs
@@ -6,7 +6,7 @@ public class Jetpack : MonoBehaviour
     [SerializeField] private Transform _comparisonTransform;
     [SerializeField] private Transform _leftHandAnchorTransform;
     [SerializeField] private Transform _rightHandAnchorTransform;
-    private Vector3 _moveForceLeft, _moveForceRight;
+    private Vector3 _moveForceLeft, _moveForceRight, _moveForce;
     protected CharacterController Controller;
 
 	/// <summary>
@@ -22,17 +22,21 @@ public class Jetpack : MonoBehaviour
     /// Calling per frame.
     /// </summary>
     private void Update()
-    {
-		// Subtract the rib cage vector with the hand vector and divide by four.
-        _moveForceLeft = (_comparisonTransform.position - _leftHandAnchorTransform.position) / 4;
-        _moveForceRight = (_comparisonTransform.position - _rightHandAnchorTransform.position) / 4;
+    {        
+        // Movement in space.
+        if (OVRInput.Get(OVRInput.Button.Three)) // Left jetpack.
+            _moveForceLeft += (_comparisonTransform.position - _leftHandAnchorTransform.position) / 80;
+        else if (OVRInput.Get(OVRInput.Button.One)) // Right jetpack.
+            _moveForceRight += (_comparisonTransform.position - _rightHandAnchorTransform.position) / 80;
 
-        // Left jetpack.
-        if (OVRInput.Get(OVRInput.Button.Three))
-            Controller.Move(_moveForceLeft);
+        // Set move force.
+        _moveForce = _moveForceLeft + _moveForceRight;
 
-        // Right jetpack.
-        if (OVRInput.Get(OVRInput.Button.One))
-            Controller.Move(_moveForceRight);
+        // Move the player with move force values.
+        Controller.Move(_moveForce);
+
+        // 0.5 per cent lost movement speed.
+        _moveForceLeft *= 0.995f;
+        _moveForceRight *= 0.995f;
     }
 }
